1d31b6a0b36a75ff72c6bd055a8133a2acada44a
DuanYu <duanyuee@gmail.com>
2020-01-08 10:09:48 +0800

    Change all for Server (many ops only be run in main thread)

--- Assets/Scripts/NetworkManagement/Message.cs
+++ Assets/Scripts/NetworkManagement/Message.cs
@@ -1,36 +1,23 @@
 ﻿using System;
 using System.Linq;
 using System.Text;
+using GameServer.Common;
+using UnityEngine;
 
 namespace GameServer {
     public class Message {
         private byte[] _data = new byte[1024];
         // 读取数据的位置 在数组里存取多少字节
         private int _startIndex = 0;
-
-        /// <summary>
-        /// 将字符串转换为字符数组
-        /// </summary>
-        /// <param name="data">需要转换的字节</param>
-        public static byte[] GetBytes(string data) {
-            // 数据内容
-            byte[] dataBytes = Encoding.UTF8.GetBytes(data);
-            int dataLength = dataBytes.Length;
-            // 数据长度
-            byte[] lengthBytes = BitConverter.GetBytes(dataLength);
-            // 将两个字符连接
-            byte[] newBytes = lengthBytes.Concat(dataBytes).ToArray();
-
-            return newBytes;
-        }
+        
 
         /// <summary>
         /// 解析读取数据
-        /// |-------|------------|-------------|------|
-        /// | Count | ActionCode | RequestCode | Data |
-        /// |-------|------------|-------------|------|
+        /// |-------|-------------|------|
+        /// | Count | RequestCode | Data |
+        /// |-------|-------------|------|
         /// </summary>
-        public void ReadMessage(int newDataAmount, Action<string> processDataCallBack) {
+        public void ReadMessage(int newDataAmount, Action<RequestCode, ActionCode, string> processDataCallBack) {
             _startIndex += newDataAmount;
             while (true) {
                 if (_startIndex <= 4) return;
@@ -38,13 +25,10 @@ namespace GameServer {
                 int count = BitConverter.ToInt32(_data, 0);
 
                 if (_startIndex - 4 >= count) {
-    //                    string s = Encoding.UTF8.GetString(_data, 4, count);
-    //                    Console.Write("解析出的数据为：" + s);
-                    // 先解析RequestCode
-    //                    RequestCode requestCode = (RequestCode)BitConverter.ToInt32(_data, 4);
-    //                    ActionCode actionCode = (ActionCode)BitConverter.ToInt32(_data, 8);
-                    string s = Encoding.UTF8.GetString(_data, 12, count-8);
-                    processDataCallBack(s);
+                   // 先解析RequestCode
+                    RequestCode requestCode = (RequestCode)BitConverter.ToInt32(_data, 4);
+//                    ActionCode actionCode = (ActionCode)BitConverter.ToInt32(_data, 8);
+                    string s = Encoding.UTF8.GetString(_data, 8, count-4);
                     // ????
                     Array.Copy(_data, count+4, _data, 0, _startIndex-4-count);
                     _startIndex -= (count + 4);
@@ -61,9 +45,26 @@ namespace GameServer {
         /// </summary>
         /// <param name="requestCode"></param>
         /// <param name="data"></param>
-        public static byte[] PackData(string data) {
-            byte[] result = Encoding.UTF8.GetBytes(data);
-            return result;
+        public byte[] PackData(RequestCode requestCode, Vector3 position) {
+            // TODO 当前只能处理一个客户端请求
+            
+            // 1. 强制转换RequestCode
+            byte[] requestCodeBytes = BitConverter.GetBytes((int)requestCode);
+            
+            // 2. 将位置转换为bytes类型
+            byte[] x = BitConverter.GetBytes(position.x);
+            byte[] y = BitConverter.GetBytes(position.y);
+            byte[] z = BitConverter.GetBytes(position.z);
+            
+            // 3. 长度为12字节
+            var positionBytes = x.Concat(y).Concat(z).ToArray();
+            
+            // 4. 固定长度为16字节
+            int dataAmount = requestCodeBytes.Length + positionBytes.Length;
+            byte[] dataAmountBytes = BitConverter.GetBytes(dataAmount);
+            
+            // 打包好的字符串长度为20字节
+            return dataAmountBytes.Concat(requestCodeBytes).Concat(positionBytes).ToArray();
         }
         
         public byte[] Data {
--- Assets/Scripts/NetworkManagement/NetworkManagement.cs
+++ Assets/Scripts/NetworkManagement/NetworkManagement.cs
@@ -1,97 +1,105 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using GameServer;
+using GameServer.Common;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
-public class NetworkManagement : MonoBehaviour{
+public class NetworkManagement : MonoBehaviour {
     
-    private Socket server;
+    private Socket _server;
     private List<Socket> _clientList;
-    private List<string> clientPosition;
-    public GameObject clientPrefab;
-    public NetworkManagement()
-    {
-        server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+    private List<string> _clientPosition;
+
+    public void Start() {            
+        _server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
         _clientList = new List<Socket>();
-        clientPosition = new List<string>();
-    }
-
-    public void Start() {
-//        StartHost();
-        StartClient();
-    }
-
-    public void StartHost() {
-        server.Bind(new IPEndPoint(IPAddress.Any, 12345));
-        server.Listen(10);
+        _clientPosition = new List<string>();
+        
+        _server.Bind(new IPEndPoint(IPAddress.Any, 12345));
+        _server.Listen(10);
         Debug.Log("服务器启动成功...");
 
-        Thread acceptThread = new Thread(AcceptFun);
+        var acceptThread = new Thread(AcceptFun);
         acceptThread.IsBackground = true;
         acceptThread.Start();
     }
 
-    public void StartClient() {
-        Instantiate(clientPrefab);
-    }
+    private void AcceptFun() {
+        while (true) {
+            Socket client = _server.Accept();
+            IPEndPoint clientPoint = client.RemoteEndPoint as IPEndPoint;
+            Debug.Log(clientPoint.Address + "[" + clientPoint.Port + "]:" + "连接成功...");
 
-    private void AcceptFun()
-    {
-        while (true)
-        {
-            Socket client = server.Accept();
             _clientList.Add(client);
+            _clientPosition.Add("_0_0_0*0*0*0");
+            if (_clientList.Count > 1) {
+                client.Send(Encoding.UTF8.GetBytes("!" + _clientList.Count));
+                for (int i = 1; i < _clientList.Count; i++) {
+                    client.Send(Encoding.UTF8.GetBytes("|" + _clientPosition[i - 1]));
+                }
+                Broadcast(client, "@newCreated");
+            }
+
             Thread receiveThread = new Thread(()=>ReceiveFun(client));
             receiveThread.IsBackground = true;
             receiveThread.Start();
         }
     }
 
-    private void ReceiveFun(Socket client)
+    private void ReceiveFun(Socket obj)
     {
+        Socket client = obj;
+        IPEndPoint clientPoint = client.RemoteEndPoint as IPEndPoint;
         byte[] msgByte = new byte[1024];
         string msgStr = "";
-       
         while (true)
         {
             try
             {
-                client.Receive(msgByte);
-//                Debug.Log(BitConverter.ToSingle(msgByte, 0));
-                BoardCast(client, msgByte);
-//                client.Send(msgByte);
+                int msgLen = client.Receive(msgByte);
+                if (msgLen > 0)
+                {
+                    msgStr = Encoding.UTF8.GetString(msgByte, 0, msgLen);
+                    if (msgStr[0] == '_')
+                    {
+                        int index = _clientList.FindIndex(item => item.Equals(client));
+                        _clientPosition[index] = msgStr;
+                        Console.WriteLine(msgStr);
+                        Broadcast(client, msgStr + "|" + index.ToString());
+                    }
+                    else
+                    {
+                        Console.WriteLine(msgStr);
+                        Broadcast(client, clientPoint.Address + "[" + clientPoint.Port + "]:" + msgStr);
+                    }
+                }
+                client.Send(Encoding.UTF8.GetBytes("$ACK"));                    
             }
             catch (Exception e)
             {
-                Debug.Log(e.Message);
+                int index = _clientList.FindIndex(item => item.Equals(client));
+                _clientPosition.RemoveAt(index);
+                _clientList.Remove(client);
+                Broadcast(client, "#oneDestroyed_" + (index + 1).ToString());
+                Console.WriteLine(e.Message);
                 break;
             }
         }           
     }
 
-    /// <summary>
-    /// 向其他客户端广播数据
-    /// </summary>
-    /// <param name="client"></param>
-    /// <param name="message"></param>
-    private void BoardCast(Socket client, byte[] message) {
-        foreach (var otherClient in _clientList) {
-            if (client != otherClient) {
-                otherClient.Send(message);
+    private void Broadcast(Socket clientID, string msg)
+    {
+        foreach (var item in _clientList)
+        {
+            if (item != clientID)
+            {
+                item.Send(Encoding.UTF8.GetBytes(msg));
             }
-            
-        }
-    }
-
-    private void OnApplicationQuit() {
-        server.Close();
-        foreach (var client in _clientList) {
-            client.Close();
         }
     }
 }
